5bc83df3d4338fd090fb013e0a3f7ca94df65e96
charvipandey <charvu.pandey@gmail.com>
2024-06-04 15:36:45 +0530

    practiced array manipulation

--- JavaScript/concept1.js
+++ JavaScript/concept1.js
@@ -1,12 +1,7 @@
-function add()
-{
-  let sum = 0;
-  let arr = Array.from(arguments);
-  arr.forEach(element => {
-      sum = sum + element;
-  });
-  console.log(sum);
-}
-add(2,3,4,5);
-add(2,3);
-add(1,2,3,4,5,6,7,8,9,10);
+let arr1 = [1,2,3,4,5];
+let arr2 = [34,45,56,67,89];
+let arr3 = [2,3,4,5,6];
+let arr = [arr1,arr2,arr3];
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[0][2]);
--- JavaScript/concept2.js
+++ JavaScript/concept2.js
@@ -1,18 +1,6 @@
-(function getDigitalTime()
-{
-  let dt = new Date();
-  let hours = dt.getHours();
-  let minutes = dt.getMinutes();
-  let seconds =  dt.getSeconds();
-  let hour12 = hours>12? hours-12 : hours;
-  if(hours == 0)
-  {
-    hour12 = "12"
-  }
-  let time="";
-  time += hour12<10?"0"+hour12:""+hour12;
-  time += minutes<10?":0"+minutes:":"+minutes;
-  time += seconds<10?":0"+seconds:":"+seconds;
-  time += hours>12? " PM" : " AM";
-  console.log(time);
-})()
+const str = "Hello my name is Charvi.";
+const lowerStr = str.toLowerCase();
+console.log(lowerStr);
+const upperStr = str.toUpperCase();
+console.log(upperStr);
+const trimStr = str.trim();
