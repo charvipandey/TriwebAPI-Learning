1309c81561eeb067eff36cfa938f2e44f48bba7d
charvipandey <charvu.pandey@gmail.com>
2024-06-18 15:19:26 +0530

    practiced polymorphism

--- NodeJS/file4.js
+++ NodeJS/file4.js
@@ -1,40 +1,33 @@
-class shape {
-  constructor() {}
-
-  area() {
-      return 0;
+class animal {
+  speak() {
+      console.log("the animal makes a sound");
   }
 }
 
-class circle extends shape {
-  constructor(radius) {
-      super();
-      this.radius = radius;
-  }
-
-  area() {
-      return Math.PI * this.radius * this.radius;
+class dog extends animal {
+  speak() {
+      console.log("the dog barks");
   }
 }
 
-class rectangle extends shape {
-  constructor(width, height) {
-      super();
-      this.width = width;
-      this.height = height;
+class cat extends animal {
+  speak() {
+      console.log("the cat meows");
   }
+}
 
-  area() {
-      return this.width * this.height;
-  }
+function makeSpeak(anml) {
+  anml.speak();
 }
 
 function main() {
-  const circ = new circle(5);
-  console.log(`area of circle with radius 5: ${circ.area()}`);
+  const anml = new animal();
+  const dg = new dog();
+  const ct = new cat();
 
-  const rect = new rectangle(4, 6);
-  console.log(`area of rectangle with width 4 and height 6: ${rect.area()}`);
+  makeSpeak(anml);
+  makeSpeak(dg);
+  makeSpeak(ct);
 }
 
 main();
