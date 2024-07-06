689f3b4c868c577d1f52943f132eb52c6a881d18
charvipandey <charvu.pandey@gmail.com>
2024-07-06 12:43:55 +0530

    added database configuration

--- Projects/quiz-app/app.js
+++ Projects/quiz-app/app.js
@@ -1,8 +1,14 @@
 const express=require('express');
+const mongoose=require('mongoose');
+const dbConnect = require('./config/db');
+dbConnect();
 const app=express();
+
+app.use(express.json());
+
 app.use((req, res, next)=>{
   res.status(200).json({
-    message: 'Welcome to the servr'
+    message: 'Welcome to the server'
   });
 });
 module.exports=app;
---
+++ Projects/quiz-app/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/quiz-app', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('mongodb is connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
