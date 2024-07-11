785b28c350b95e3316d3dcc52b14d552949470e3
charvipandey <charvu.pandey@gmail.com>
2024-07-11 13:14:32 +0530

    updated basic styling

--- Projects/quiz-frontend/src/App.js
+++ Projects/quiz-frontend/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Register from './components/Register';
+import Login from './components/Login';
+import UserProfile from './components/UserProfile';
+import QuizCRUD from './components/QuizCRUD';
+import QuizAttempt from './components/QuizAttempt';
+import Reports from './components/Reports';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+        {}
+        <Register />
+        <Login />
+        <UserProfile />
+        <QuizCRUD />
+        <QuizAttempt />
+        <Reports />
+      </main>
+      <Footer />
     </div>
   );
 }
